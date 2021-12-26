9105a87644896883e18a3d9131b6195d33f90e24
zionlee0927 <zionlee0927@tpirates.com>
2021-12-26 15:37:02 +0900

    1152 init

---
+++ src/Main/A_string/E_1152.java
@@ -0,0 +1,18 @@
+package Main.A_string;
+
+import java.util.Scanner;
+
+// 5. 단어의 개수
+public class E_1152 {
+    public void solution(String line) {
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+
+    public static void main(String[] args) {
+        E_1152 M = new E_1152();
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        M.solution(s);
+    }
+}
