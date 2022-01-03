51375cf8b2698cfc5a7214406153e225c2977154
zionlee0927 <zionlee0927@gmail.com>
2022-01-03 23:46:38 +0900

    D_2869 clear

--- src/Main/B_basic_math_1/D_2869.java
+++ src/Main/B_basic_math_1/D_2869.java
@@ -1,11 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+package Main.B_basic_math_1;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Main M = new Main();
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 4. 달팽이는 올라가고 싶다
+public class D_2869 {
+    public static void main(String[] args) throws Exception{
+        D_2869 M = new D_2869();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,7 +20,7 @@ public class Main {
         System.out.println(M.solution(a,b,v));
     }
 
-    private int solution(int a, int b, int v) {
+    private static int solution(int a, int b, int v) {
         int day = (v - b) / (a - b);
         if ((v - b) % (a - b) != 0)
             day++;
