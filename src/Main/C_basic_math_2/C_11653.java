5b0b8542625100667ea38568a5967ee3f134b265
zionlee0927 <zionlee0927@gmail.com>
2022-01-16 14:15:17 +0900

    기본수학2 : C_11653 clear

--- src/Main/C_basic_math_2/C_11653.java
+++ src/Main/C_basic_math_2/C_11653.java
@@ -1,9 +1,14 @@
-import java.io.IOException;
-import java.util.*;
+package Main.C_basic_math_2;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Main M = new Main();
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+// 3. 소인수분해
+public class C_11653 {
+    public static void main(String[] args) {
+        C_11653 M = new C_11653();
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
