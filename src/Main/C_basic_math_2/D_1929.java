a5ed21da04279f2eacf17f7cfbeeecfba2cba390
zionlee0927 <zionlee0927@gmail.com>
2022-01-18 20:57:03 +0900

    기본수학2 : D_1929 init

---
+++ src/Main/C_basic_math_2/D_1929.java
@@ -0,0 +1,20 @@
+package Main.C_basic_math_2;
+
+import java.util.Scanner;
+
+// 4. 소수 구하기
+public class D_1929 {
+    public static void main(String[] args) {
+        D_1929 M = new D_1929();
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(M.solution(n));
+    }
+
+    private String solution(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        return stringBuilder.toString();
+    }
+}
