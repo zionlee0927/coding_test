83a2bb099ca065a88c0eb87194a5e108d556538d
zionlee0927 <zionlee0927@gmail.com>
2022-01-22 23:55:22 +0900

    기본수학2 : F_9020 add

---
+++ src/Main/C_basic_math_2/F_9020.java
@@ -0,0 +1,17 @@
+package Main.C_basic_math_2;
+
+import java.util.Scanner;
+
+// 6. 골드바흐의 추측
+public class F_9020 {
+    public static void main(String[] args) {
+        F_9020 M = new F_9020();
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+    private int solution(int n) {
+        return 0;
+    }
+}
