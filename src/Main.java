2c1ea944556498089f82d46d76f8928a32dab32d
zionlee0927 <zionlee0927@gmail.com>
2022-01-27 23:10:01 +0900

    기본수학2 : H_3009 1 try

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,12 @@ public class Main {
         int x3 = scanner.nextInt();
         int y3 = scanner.nextInt();
 
-        System.out.println(M.solution(x1,y1,x2,y2,x3,y3));
+        System.out.print(M.solution(x1,y1,x2,y2,x3,y3));
     }
 
     private String solution(int x1, int y1, int x2, int y2, int x3, int y3) {
         int x4 = (x1 == x2) ? x3 : (x1 == x3 ? x2 : x1);
-        int y4 = (y1 == y2) ? y1 : (y1 == y3 ? y2 : y1);
+        int y4 = (y1 == y2) ? y3 : (y1 == y3 ? y2 : y1);
 
         return x4 + " " + y4;
     }
--- src/Main/C_basic_math_2/H_3009.java
+++ src/Main/C_basic_math_2/H_3009.java
@@ -21,7 +21,7 @@ public class H_3009 {
 
     private String solution(int x1, int y1, int x2, int y2, int x3, int y3) {
         int x4 = (x1 == x2) ? x3 : (x1 == x3 ? x2 : x1);
-        int y4 = (y1 == y2) ? y1 : (y1 == y3 ? y2 : y1);
+        int y4 = (y1 == y2) ? y3 : (y1 == y3 ? y2 : y1);
 
         return x4 + " " + y4;
     }
