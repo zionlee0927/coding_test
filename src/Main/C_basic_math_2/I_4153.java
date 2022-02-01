6659f051235ff0e6f0e0cbcd8fc52d3b00a76ef4
zionlee0927 <zionlee0927@gmail.com>
2022-02-01 22:53:57 +0900

    기본수학2 : I_4153 init

---
+++ src/Main/C_basic_math_2/I_4153.java
@@ -0,0 +1,19 @@
+package Main.C_basic_math_2;
+
+import java.util.Scanner;
+
+// 8. 네 번째 점
+public class I_4153 {
+    public static void main(String[] args) {
+        I_4153 M = new I_4153();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int testCase = scanner.nextInt();
+        System.out.println(M.solution(testCase));
+    }
+
+    private String solution(int n) {
+        return null;
+    }
+}
