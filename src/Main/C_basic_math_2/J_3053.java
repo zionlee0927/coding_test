f8fbbf717c2f5f24bf3a489e9e3988b0d5a9044d
zionlee0927 <zionlee0927@gmail.com>
2022-02-03 22:55:36 +0900

    기본수학2 : J_3053 init

---
+++ src/Main/C_basic_math_2/J_3053.java
@@ -0,0 +1,24 @@
+package Main.C_basic_math_2;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// 9. 택시 기하학
+public class J_3053 {
+    public static void main(String[] args) {
+        J_3053 M = new J_3053();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int b = scanner.nextInt();
+        System.out.println(M.solution(b));
+    }
+
+    private String solution(int b) {
+
+        return "wrong";
+    }
+}
