82a89b463dd03adc906592057e64ec6754e3199b
zionlee0927 <zionlee0927@gmail.com>
2022-02-04 21:51:37 +0900

    기본수학2 : J_3053 clear

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,27 +7,11 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        while (true){
-            int a = scanner.nextInt();
-            if (a == 0) break;
-            int b = scanner.nextInt();
-            int c = scanner.nextInt();
-            System.out.println(M.solution(a,b,c));
-        }
+        int r = scanner.nextInt();
+        System.out.println(M.solution(r));
     }
 
-    private String solution(int a, int b, int c) {
-
-        List<Integer> collect = Stream.of(a, b, c)
-                .sorted(Comparator.comparing(Integer::intValue))
-                .collect(Collectors.toList());
-
-        int ausar = collect.get(0);
-        int auset = collect.get(1);
-        int heru = collect.get(2);
-
-        if (Math.pow(ausar,2)+Math.pow(auset,2) == Math.pow(heru,2)) return "right";
-
-        return "wrong";
+    private String solution(int r) {
+        return Math.PI * Math.pow(r, 2) + "\n" + 2 * Math.pow(r, 2);
     }
 }
--- src/Main/C_basic_math_2/J_3053.java
+++ src/Main/C_basic_math_2/J_3053.java
@@ -13,12 +13,11 @@ public class J_3053 {
 
         Scanner scanner = new Scanner(System.in);
 
-        int b = scanner.nextInt();
-        System.out.println(M.solution(b));
+        int r = scanner.nextInt();
+        System.out.println(M.solution(r));
     }
 
-    private String solution(int b) {
-
-        return "wrong";
+    private String solution(int r) {
+        return Math.PI * Math.pow(r, 2) + "\n" + 2 * Math.pow(r, 2);
     }
 }
