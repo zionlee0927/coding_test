3343ddd4d83eb1ffc2ab59a09b5e1e7d13a64f48
zionlee0927 <zionlee0927@gmail.com>
2022-02-08 21:45:00 +0900

    재귀 : A_10872 init

---
+++ src/Main/D_recursion/A_10872.java
@@ -0,0 +1,17 @@
+package Main.D_recursion;
+
+import java.util.Scanner;
+
+// 1. 팩토리얼
+public class A_10872 {
+    public static void main(String[] args) {
+        A_10872 M = new A_10872();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(M.solution());
+    }
+
+    private int solution() {
+        return 0;
+    }
+}
