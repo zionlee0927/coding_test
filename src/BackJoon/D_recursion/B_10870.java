8b10c423ca3e1b89425662c2723d28e368975236
zionlee0927 <zionlee0927@gmail.com>
2022-02-18 22:13:25 +0900

    10.재귀함수 : B_10870 clear

--- src/BackJoon/D_recursion/B_10870.java
+++ src/BackJoon/D_recursion/B_10870.java
@@ -14,7 +14,7 @@ public class B_10870 {
 
     private int solution(int n) {
         if (n < 1) return 0;
-
+        else if (n == 1) return  1;
         return solution(n-1) + solution(n-2);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     private int solution(int n) {
         if (n < 1) return 0;
-
+        else if (n == 1) return  1;
         return solution(n-1) + solution(n-2);
     }
 }
