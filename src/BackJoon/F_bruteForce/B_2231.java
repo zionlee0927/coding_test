87bb2216c4b832a6114d8b0990a8428cdc295e6d
zionlee0927 <zionlee0927@gmail.com>
2022-03-10 23:59:42 +0900

    11.브루트포스 : B_분해합 init

--- src/BackJoon/F_bruteForce/B_2231.java
+++ src/BackJoon/F_bruteForce/B_2231.java
@@ -1,5 +1,7 @@
 package BackJoon.F_bruteForce;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // 2. 분해합
@@ -8,12 +10,17 @@ public class B_2231 {
         B_2231 M = new B_2231();
 
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        String n = scanner.next();
         System.out.println(M.solution(n));
     }
 
-    private int solution(int n) {
-
+    private int solution(String n) {
+        char[] chars = n.toCharArray();
+        List<Integer> list = new ArrayList<>();
+        for (char c:
+             chars) {
+            list.add(Integer.valueOf(String.valueOf(c)));
+        }
         return 0;
     }
 }
