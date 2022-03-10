def881a82cc61bada08260d878676ea06c271459
zionlee0927 <zionlee0927@gmail.com>
2022-03-11 00:30:12 +0900

    11.브루트포스 : B_분해합 clear

--- src/BackJoon/F_bruteForce/B_2231.java
+++ src/BackJoon/F_bruteForce/B_2231.java
@@ -10,17 +10,31 @@ public class B_2231 {
         B_2231 M = new B_2231();
 
         Scanner scanner = new Scanner(System.in);
-        String n = scanner.next();
-        System.out.println(M.solution(n));
+        String s = scanner.nextLine();
+        System.out.println(M.solution(s));
     }
 
-    private int solution(String n) {
-        char[] chars = n.toCharArray();
-        List<Integer> list = new ArrayList<>();
-        for (char c:
-             chars) {
-            list.add(Integer.valueOf(String.valueOf(c)));
+    private int solution(String s) {
+        int m = 0;
+        int n = Integer.parseInt(s);
+
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        int min = 9 * (length - 1) + Integer.parseInt(String.valueOf(chars[0]));
+
+
+        for (int i = n-min; i <= n; i++) {
+            char[] charArray = String.valueOf(i).toCharArray();
+            List<Integer> list = new ArrayList<>();
+            for (char c :
+                    charArray) {
+                list.add(Integer.parseInt(String.valueOf(c)));
+            }
+            int sum = list.stream().mapToInt(Integer::intValue).sum();
+
+            if (i+sum == n) return i;
         }
-        return 0;
+
+        return m;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,31 @@ public class Main {
         Main M = new Main();
 
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        List<Integer> nums = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            nums.add(scanner.nextInt());
-        }
-        System.out.println(M.solution(m,nums));
+        String s = scanner.nextLine();
+        System.out.println(M.solution(s));
     }
 
-    private int solution(int m, List<Integer> nums) {
-        int result = 0;
+    private int solution(String s) {
+        int m = 0;
+        int n = Integer.parseInt(s);
 
-        List<Integer> reverse_sorted = nums.stream().sorted(Comparator.comparing(Integer::intValue).reversed()).collect(Collectors.toList());
-        Integer min = reverse_sorted.get(reverse_sorted.size() - 1);
-        Integer next_min = reverse_sorted.get(reverse_sorted.size() - 2);
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        int min = 9 * (length - 1) + Integer.parseInt(String.valueOf(chars[0]));
 
-        for (int i = 0; i < reverse_sorted.size() - 3; i++) {
-            if (reverse_sorted.get(i) > m - min - next_min) reverse_sorted.remove(i);
-        }
 
-        for (int i = 0; i < reverse_sorted.size()-2; i++) {
-            for (int j = i+1; j < reverse_sorted.size()-1; j++) {
-                for (int k = j+1; k < reverse_sorted.size(); k++) {
-                    int sum = reverse_sorted.get(i) + reverse_sorted.get(j) + reverse_sorted.get(k);
-                    if (sum==m) return sum;
-                    if (sum <= m && m-sum < m-result) result = sum;
-                }
+        for (int i = n-min; i <= n; i++) {
+            char[] charArray = String.valueOf(i).toCharArray();
+            List<Integer> list = new ArrayList<>();
+            for (char c :
+                    charArray) {
+                list.add(Integer.parseInt(String.valueOf(c)));
             }
+            int sum = list.stream().mapToInt(Integer::intValue).sum();
+
+            if (i+sum == n) return i;
         }
-        return result;
+
+        return m;
     }
 }
