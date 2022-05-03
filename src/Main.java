f76a02b36fcccf6f957a1d85e19adcd0dfd18c70
zionlee0927 <zionlee0927@gmail.com>
2022-05-03 17:22:01 +0900

    11.브루트포스 : D_1078 pass

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws IOException {
         Main M = new Main();
-
         Scanner scanner = new Scanner(System.in);
         int w = scanner.nextInt();
         int h = scanner.nextInt();
